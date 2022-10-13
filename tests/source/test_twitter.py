f74795ed5e5ae002fa3ac9f50c0219ad9167ce72
Alicia Bargar <aliciabargar@gmail.com>
2022-10-13 09:32:46 +0200

    stubs

--- tests/source/test_twitter.py
+++ tests/source/test_twitter.py
@@ -1,2 +1,26 @@
-def test__init():
-    assert 1 == 1
+## TwitterWrapper
+
+
+def test__get_authentication():
+    pass
+
+
+def test__home_timeline():
+    pass
+
+
+def test__search():
+    pass
+
+
+## TwitterStreamClient
+def test__add_rules():
+    pass
+
+
+def test__on_tweet():
+    pass
+
+
+def test__cleanup_rules():
+    pass
